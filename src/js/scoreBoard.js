f0b7eb839e877e3b4a617d8e85f14431892f0d03
saltymouse <saltymouse@outlook.com>
2017-03-10 19:51:20 +0900

    create a player constructor to allow creating multiple players

--- src/js/scoreBoard.js
+++ src/js/scoreBoard.js
@@ -1,169 +1,207 @@
 console.log('loaded scoreBoard.js');
 
+var app = document.querySelector('.app');
+var stage = document.querySelector('.stage');
 var header = document.querySelector('header');
+var addPlayerButton = document.querySelector('.add-player');
+var removePlayerButton = document.querySelector('.remove-player');
+var players = [];
 
-var player1 = document.querySelector('.player1');
-var player2 = document.querySelector('.player2');
+window.addEventListener('scroll', function(e) {
+  if (window.pageYOffset > players[0].elements.playerHeading.scrollHeight) {
+    players.forEach(function(player) {
+      player.elements.playerHeading.className = 'name squish'
+    })
+  } else if (window.pageYOffset < players[0].elements.playerHeading.scrollHeight) {
+    players.forEach(function(player) {
+      player.elements.playerHeading.className = 'name'
+    })
+  }
+})
 
-var player1Total = document.querySelector('.player1 .total');
-var player2Total = document.querySelector('.player2 .total');
 
-var player1Add = document.querySelector('.player1 .add');
-var player2Add = document.querySelector('.player2 .add');
+removePlayerButton.addEventListener('click', removePlayer);
+addPlayerButton.addEventListener('click', pushPlayer);
 
-var player1Input = document.querySelector('.player1 input');
-var player2Input = document.querySelector('.player2 input');
+function pushPlayer(playerName) {
+  var args = Array.prototype.slice.apply(arguments);
+  console.log(args);
+  args = args.slice(1);
+  console.log(args);
+  if (players.length <= 3) { // only 4 players max
+    players.push(new Player(playerName));
+    players.forEach(function(player) {
+      player.initialise();
+    })
+  }
+}
 
-var player1ScoreList = document.querySelector('.player1 ul');
-var player2ScoreList = document.querySelector('.player2 ul');
+function removePlayer() {
+  if (players.length > 1) { //at least 1 player
+    players[players.length -1].erase();
+    players.splice(-1, 1);
+  }
+}
 
-header.addEventListener('click', reset, false);
+function randomEmoji() {
+  return 1;
+}
+console.log(randomEmoji());
 
-function Player() {
+function Player(playerName) {
   this.scores = [],
   this.total = 0,
-  this.add = function() {
-    console.log('add');
-  }
-}
+  this.name = playerName ? playerName : 'P' + Math.floor(Math.random() * 99),
+  this.elements = {
+    playerWrap: document.createElement('section'),
+    playerHeading: document.createElement('h2'),
+    playerTitle: document.createElement('span'),
+    inputWrap: document.createElement('div'),
+    scoreInput: document.createElement('input'),
+    addButton: document.createElement('button'),
+    scores: document.createElement('ul'),
+    total: document.createElement('div')
+  },
+  this.initialise = function() {
+    this.prepareElements();
+    this.appendElements();
+    this.refresh();
+  },
+  this.prepareElements = function () {
+    // add appropriate classes to each element
+    this.elements.playerWrap.className = 'player' + ' ' + this.name;
+    this.elements.playerHeading.className = 'name';
+    this.elements.playerTitle.className = 'title';
+    this.elements.inputWrap.className = 'input-wrap';
+    this.elements.scoreInput.className = 'score-input';
+    this.elements.addButton.className = 'add';
+    this.elements.scores.className = 'scores';
+    this.elements.total.className = 'total';
 
-var p1 = new Player();
+    // add custom attributes where necessary
+    this.elements.playerTitle.innerHTML = this.name;
+    this.elements.playerTitle.contentEditable = true;
 
-var player1Scores = [];
-var player2Scores = [];
+    this.elements.addButton.innerHTML = '+';
+    this.elements.addButton.addEventListener('click', this.addToScores.bind(this), false); // bind 'this' to the outer Player object
+    this.elements.scoreInput.addEventListener('keydown', function(event) {
+      if (event.keyCode === 13) {
+        this.addToScores();
+      }
+    }.bind(this), false); // bind 'this' to the outer Player object
 
-player1Add.addEventListener('click', addPlayer1Score, false);
-player2Add.addEventListener('click', addPlayer2Score, false);
+    this.elements.scoreInput.type = 'number';
+    this.elements.scoreInput.pattern = '[0-9]';
+  },
+  this.appendElements = function() {
+    // first nest the appropriate elements into their parents
+    // start with smaller components...
+    this.elements.playerHeading.appendChild(this.elements.playerTitle);
 
-function addPlayer1Score() {
-  player1Scores.push( Number(player1Input.value) );
-  if (player1Scores !== "" && player1Scores !== NaN) {
+    this.elements.inputWrap.appendChild(this.elements.scoreInput);
+    this.elements.inputWrap.appendChild(this.elements.addButton);
 
-    // wipe the html clean before re-rendering
-    player1ScoreList.innerHTML = '';
+    // ...add smaller components to the main player wrapper
+    this.elements.playerWrap.appendChild(this.elements.playerHeading);
+    this.elements.playerWrap.appendChild(this.elements.inputWrap);
+    this.elements.playerWrap.appendChild(this.elements.scores);
+    this.elements.playerWrap.appendChild(this.elements.total);
 
-    // render the scores and rounds to html
-    player1Scores.forEach( function(score, index) {
+    // then create a big bundle/staging area to push into the dom
+    var bundle = document.createDocumentFragment();
+    bundle.appendChild(this.elements.playerWrap);
 
-      var deleteBox = document.createElement('span');
-      deleteBox.innerHTML = '&times;'
-      deleteBox.className = 'delete'
+    // push the bundle into the dom
+    stage.appendChild(bundle);
+  },
+  this.refresh = function() {
+    this.clearInputBox();
+    this.clearScoresDisplay();
+    this.displayScores();
+    this.totalScores();
+    this.displayTotal();
+  },
+  this.setName = function() {
+    this.elements.playerName.innerHTML = playerName || 'Player';
+  },
+  this.addToScores = function(apiInput) {
+    var guiInput = this.elements.scoreInput.value,
+        sanitizedInput;
 
-      // create span for the round number and add 'round' class
-      var currentRoundElement = document.createElement('span');
-      currentRoundElement.className = 'round';
-      // insert the current round number into the element
-      currentRoundElement.innerHTML = index + 1;
+    // sanitize input to only get numbers
+    if ( !isNaN(parseFloat(guiInput)) && isFinite(guiInput) ) {
+      sanitizedInput = Number(guiInput);
+    } else if ( !isNaN(parseFloat(apiInput)) && isFinite(apiInput) ) {
+      sanitizedInput = Number(apiInput);
+    }
 
-
-      // create span for the current score and add 'score' class
-      var currentScoreElement = document.createElement('span');
-      currentScoreElement.className = 'score';
-      currentScoreElement.contenteditable = true;
-      // put the current score into the 'score' element
-      currentScoreElement.innerHTML = score;
-
-      // create a new list element to house the above two 'round' & 'score'
-      var newScoreListItem = document.createElement('li');
-      newScoreListItem.addEventListener('click', function(e) {
-        if (e.target.className.match(/round/g) && !e.target.parentNode.className.match(/remove/g)) {
-          e.target.parentNode.className += ' remove';
-        } else if (e.target.className.match(/round/g)) {
-          e.target.parentNode.className = 'round';
-        } else if (e.target.className.match(/delete/g)) {
-          player1ScoreList.removeChild(e.target.parentNode)
-          console.log(player1Scores.splice(index, 1));
-        }
-        console.log(e.target.className);
-        var deleteBox = document.createElement('span');
-        deleteBox.className = "box delete";
-
+    // only add score if sanitizedInput exists (i.e. is a number including 0)
+    if (sanitizedInput || sanitizedInput === 0) {
+      this.scores.push(sanitizedInput);
+    }
+    this.refresh();
+  },
+  this.removeScore = function(scoreIndex) {
+    this.scores.splice(scoreIndex, 1);
+    this.refresh();
+  },
+  this.totalScores = function() {
+    if (this.scores.length > 0) {
+      this.total = this.scores.reduce(function(tempTotal, score) {
+        return tempTotal + score;
       })
+    }
+  },
+  this.displayTotal = function() {
+    if (this.scores.length > 0) {
+      this.elements.total.innerHTML = this.total;
+    } else {
+      this.elements.total.innerHTML = '◽️';
+    }
+  },
+  this.displayScores = function() {
+    this.scores.forEach(function(scoreValue, roundValue) {
+      // create necessary elements for each score value on display
+      var scoreWrap = document.createElement('li');
+      var scoreBox = document.createElement('span');
+      var roundBox = document.createElement('span');
 
-      // bundle the 'round' and 'score' into the new <li> element
-      newScoreListItem.appendChild(deleteBox);
-      newScoreListItem.appendChild(currentRoundElement);
-      newScoreListItem.appendChild(currentScoreElement);
+      // add css classes
+      scoreWrap.className = 'round-wrap';
+      scoreBox.className = 'score';
+      roundBox.className = 'round';
 
-      // append the element to the parent list ul
-      player1ScoreList.appendChild(newScoreListItem);
+      // add the values
+      scoreBox.innerHTML = scoreValue;
+      roundBox.innerHTML = roundValue + 1; // +1 for humans!
 
+      // add remove
+      roundBox.addEventListener('click', function(event) {
+        var clickedRound = event.target.innerHTML - 1;
+        this.removeScore(clickedRound);
+      }.bind(this));
 
+      // nest inside the wrapper
+      scoreWrap.appendChild(roundBox);
+      scoreWrap.appendChild(scoreBox);
 
-    });
-
-    // clear input field after adding to list
-    player1Input.value = "";
-
-    // sum the score
-    var total;
-    player1Total.innerHTML = "Total: " + player1Scores.reduce(function (a, b) {
-      return a + b;
-    });
-
-  }
-}
-
-function addPlayer2Score() {
-  player2Scores.push( Number(player2Input.value) );
-  if (player2Scores !== "" && player2Scores !== NaN) {
-
-    // wipe the html clean before re-rendering
-    player2ScoreList.innerHTML = '';
-
-    // render the scores and rounds to html
-    player2Scores.forEach( function(score, index) {
-
-      // create span for the round number and add 'round' class
-      var currentRoundElement = document.createElement('span');
-      currentRoundElement.className = 'round';
-      // insert the current round number into the element
-      currentRoundElement.innerHTML = index + 1;
-
-
-      // create span for the current score and add 'score' class
-      var currentScoreElement = document.createElement('span');
-      currentScoreElement.className = 'score';
-      // put the current score into the 'score' element
-      currentScoreElement.innerHTML = score;
-
-      // create a new list element to house the above two 'round' & 'score'
-      var newScoreListItem = document.createElement('li');
-
-      // bundle the 'round' and 'score' into the new <li> element
-      newScoreListItem.appendChild(currentRoundElement);
-      newScoreListItem.appendChild(currentScoreElement);
-
-      // append the element to the parent list ul
-      player2ScoreList.appendChild(newScoreListItem);
-
-    });
-
-    // clear input field after adding to list
-    player2Input.value = "";
-
-    // sum the score
-    var total;
-    player2Total.innerHTML = "Total: " + player2Scores.reduce(function (a, b) {
-      return a + b;
-    });
-
+      // append to the dom
+      this.elements.scores.appendChild(scoreWrap);
+    }, this) // 'this' for the outer Player object
+  },
+  this.clearScoresDisplay = function() {
+    this.elements.scores.innerHTML = '';
+  },
+  this.clearInputBox = function() {
+    this.elements.scoreInput.value = '';
+  },
+  this.erase = function() {
+    this.elements.playerWrap.remove();
   }
 }
 
 
-
-function player2sum(newScore) {
-  player2stats.total += Number(newScore);
-  player2total.innerHTML = "Total: " + player2stats.total;
-}
-
-function reset() {
-  player1ScoreList.innerHTML = '';
-  player1Total.innerHTML = '';
-  player1Scores = [];
-
-  player2ScoreList.innerHTML = '';
-  player2Total.innerHTML = '';
-  player2Scores = [];
-}
+pushPlayer('🦊');
+pushPlayer('🍡');
+pushPlayer('🍝');
+pushPlayer('🐙');
